b0c38d27963ba0a0110e564bbcd04e99551aec0e
Ivan Jovkovic <30907760+jokac88@users.noreply.github.com>
2023-03-11 21:11:26 +0100

    Updates

---
+++ _nuxt/app.config.b66f5c66.js
@@ -0,0 +1,1 @@
+import{l as n,G as o,H as r}from"./entry.d3c63d55.js";function i(e,t){return n()._useHead(e,t)}const s=o({title:"Hello Nuxt",theme:{dark:!0,colors:{primary:"#ff0000"}}}),u={};r(s,u);export{i as u};
---
+++ _nuxt/odgovor.5592ca6a.js
@@ -0,0 +1,1 @@
+import{a as o,o as e,b as r}from"./entry.d3c63d55.js";const t={name:"odgovor"};function n(a,c,s,p,_,d){return e(),r("h1",null,"Odgovor")}const l=o(t,[["render",n]]);export{l as default};
