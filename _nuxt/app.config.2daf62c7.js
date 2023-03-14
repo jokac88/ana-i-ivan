08583e057ef4f0c3f11d0862dd62290cba3b853f
Ivan Jovkovic <30907760+jokac88@users.noreply.github.com>
2023-03-14 14:01:49 +0100

    Updates

--- _nuxt/app.config.2daf62c7.js
+++
@@ -1,1 +0,0 @@
-import{l as u,G as s}from"./entry.c25e67fc.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
---
+++ _nuxt/app.config.52ad22ce.js
@@ -0,0 +1,1 @@
+import{l as u,G as s}from"./entry.9a25d842.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
