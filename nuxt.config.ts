a76d8e2a52ddb335c2b3c726f4880856d77585a3
jokac88 <ivan.jovkovic88@gmail.com>
2026-02-14 02:04:22 +0100

    Update nuxt to the lts version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,60 +1,58 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-    devtools: {
-        enabled: true
-    },
-    ssr: false,
-    app: {
-        baseURL: process.env.NODE_ENV === 'production' ? '/ana-i-ivan/' : '/',
-        buildAssetsDir: process.env.NODE_ENV === 'production' ? 'assets' : '',
-        head: {
-            title: 'Ана и Иван',
-            meta: [
-                {
-                    name: 'description',
-                    content: 'Ана и Иван'
-                },
-                {
-                    name: 'keywords',
-                    content: 'Ана, Иван'
-                },
-                {
-                    name: 'theme-color',
-                    content: '#577567'
-                }
-            ],
-            link: [
-                {
-                    rel: "preconnect",
-                    href:
-                        "https://fonts.googleapis.com"
-                },
-                {
-                    rel: "preconnect",
-                    href:
-                        "https://fonts.gstatic.com"
-                },
-                {
-                    rel: "stylesheet",
-                    href:
-                        "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Roboto+Slab:wght@400;500;700&display=swap"
-                },
-                {
-                    rel: 'icon',
-                    type: 'image/x-icon',
-                    href: 'favicon.ico'
-                }
-            ],
-            noscript: [
-                {
-                    innerHTML: 'JavaScript is required'
-                }
-            ]
+  compatibilityDate: '2025-07-15',
+  devtools: {
+    enabled: true
+  },
+  ssr: false,
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/ana-i-ivan/' : '/',
+    buildAssetsDir: process.env.NODE_ENV === 'production' ? 'assets' : '',
+    head: {
+      title: 'Ана и Иван',
+      meta: [
+        {
+          name: 'description',
+          content: 'Ана и Иван'
         },
+        {
+          name: 'keywords',
+          content: 'Ана, Иван'
+        },
+        {
+          name: 'theme-color',
+          content: '#577567'
+        }
+      ],
+      link: [
+        {
+          rel: "preconnect",
+          href:
+            "https://fonts.googleapis.com"
+        },
+        {
+          rel: "preconnect",
+          href:
+            "https://fonts.gstatic.com"
+        },
+        {
+          rel: "stylesheet",
+          href:
+            "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Roboto+Slab:wght@400;500;700&display=swap"
+        },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: 'favicon.ico'
+        }
+      ],
+      noscript: [
+        {
+          innerHTML: 'JavaScript is required'
+        }
+      ]
     },
-    modules: [
-        'nuxt-lodash'
-    ],
-    css: ['~/assets/scss/main.scss']
+  },
+  css: ['@/assets/scss/main.scss']
 });
